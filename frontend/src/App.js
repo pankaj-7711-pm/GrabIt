6f97a5e7520a9687e2a9a856919319affb1125e6
Prashant Tiwari <96342610+Pacificon@users.noreply.github.com>
2024-03-12 01:32:36 +0530

    Added css, removed proxy,

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      This is prashant tiwari
-      <div className="nav">Navbar</div>
+      This is prashant Tiwari
     </div>
   );
 }
