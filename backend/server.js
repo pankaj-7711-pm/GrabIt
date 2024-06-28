bb5efe8ca8b889519d7160f99e2facfa1ec64761
pankaj-7711-pm <pankajmandalplt58@gmail.com>
2024-06-29 02:52:01 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,24 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to Advertisement app</h1>");
 });
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running Successfully");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, console.log("server started in port 4000"));
